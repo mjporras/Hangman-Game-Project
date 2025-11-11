6b673ffba835b93c23d704ce98985eb7a72c44b4
Valentina Chavez <chavezgval@gmail.com>
2025-11-11 18:19:53 +0100

    update

--- Phase3.py
+++ Phase3.py
@@ -1,12 +1,55 @@
-#Check if letter is in the word
-word = ("pineapple")
-hint = ["_"]*len(word)
-print((f"The word has {len(word)} letters."),hint)
+#Stage 1: MJ CODE HERE:
+#######################################################
+def start_game():
+    word = ("pineapple")
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    lives = 6
+    hint = ["_"]*len(word)
+    print((f"The word has {len(word)} letters."),hint)
 
-letter = input("Guess a letter").lower()
+    while True:
+        letter = input("Guess a letter").lower()
 
-if letter in word:
-    for i in range(len(word)):
-        if word[i] == letter:
-            hint[i] = letter
-            print(hint)
+#FLORENCIA CODE HERE: 
+#######################################################
+
+
+
+#If letter is already guessed
+
+        if letter not in alphabet:
+            print(f"That letter {letter} was already used. Try with another letter.")
+            continue
+
+#If letter is in the word
+
+        if letter in word:
+            for i in range(len(word)):
+                if word[i] == letter:
+                    hint[i] = letter
+                    print(hint)
+
+#Remove letter from the alphabet
+
+                alphabet = alphabet.replace(letter, "")
+                print(alphabet)
+                continue
+
+#LILIAN CODE HERE:
+#######################################################
+#If letter is not in the word
+
+
+
+#Check if the word is complete
+        if lives !=0 and "_" in hint:
+            print(f"You won! The word is: {word}")
+            play_again = input("Do you want to play again? (yes/no): ")
+            if play_again.lower() =="yes":
+                break
+            #Go back to phase 1 - start_game()
+        else:
+            print("Thank you for playing!")
+            break
+
+    #Go to phase 2
